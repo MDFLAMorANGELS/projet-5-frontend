ecf820c1632584b4f4c0e7b5780db848af255bf6
PvalentinOC <76172386+PvalentinOC@users.noreply.github.com>
2021-06-28 16:49:49 +0200

    correction de la page produit afin que les infos s affiche selon id qui est dans l url et creation d une fonction

--- produit.js
+++ produit.js
@@ -2,40 +2,25 @@
 //recuperer le produit en fonction de son id avec l API
 //afficher le produit
 //permettre d afficher la couleur et d ajouter au panier
-fetch("http://localhost:3000/api/teddies")
+let id = getParamFromURI('id');
+if ( id !== undefined){
+    fetch("http://localhost:3000/api/teddies/"+id)
     .then(res => res.json())
   .then(produit => {
     console.log(produit);
-  })
-
-
-let searchParams = new URL(window.location).searchParams.get('id');
-console.log(searchParams);
-if (searchParams.has('id')) {
-    let prodId = searchParams.get('id');
-    let produitToDisplay = produit[prodId];
     let root = document.querySelector('#product-detail');
-    root.innerHTML = Produit.displayInList();
     let prod = new Produit(produit);
     root.innerHTML = prod.displayInList();
+  })
 } else {
-    windows.location.pathname = 'index.hmtl';
+    let root = document.querySelector('#product-detail');
+    root.innerHTML = `<h1>Pas de Produit Selectionne</h1>`
 }
-fetch("http://localhost:3000/api/teddies")
-    .then(res => res.json())
-    .then(product => {
-    console.log(product);
-    if (id === Produit.elt._id) {
-        return displayInList()
+
+function getParamFromURI(param){
+    let searchParams = new URL(window.location).searchParams;
+    if(searchParams.has(param)){
+        return searchParams.get(param);
     }
-    })
-
-/*async function myFetch() {
-//    let response = await fetch(`http://localhost:3000/api/teddies/${id}`);
-}*/
-
-
-//const idProduitSelectionner = id.find((element) => element._id === id);
-
-let mainProduit = document.getElementById("product-detail");
-mainProduit =  Produit.displayInList();
+    return undefined;
+}
