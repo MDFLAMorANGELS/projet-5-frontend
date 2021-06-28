7c3b61a1de09e5c5d42a22de2337659c33b6433d
PvalentinOC <76172386+PvalentinOC@users.noreply.github.com>
2021-06-28 12:37:15 +0200

    Update produit.js

--- produit.js
+++ produit.js
@@ -5,22 +5,20 @@
 let id= new URL(window.location).searchParams.get("id");
 console.log(id);
 
-async function myFetch() {
-    let response = await fetch(`http://localhost:3000/api/teddies/${id}`);
-}
+fetch("http://localhost:3000/api/teddies")
+    .then(res => res.json())
+    .then(product => {
+    console.log(product);
+    if (id === Produit.elt._id) {
+        return displayInList()
+    }
+    })
+/*async function myFetch() {
+//    let response = await fetch(`http://localhost:3000/api/teddies/${id}`);
+}*/
 
 
 //const idProduitSelectionner = id.find((element) => element._id === id);
 
 let mainProduit = document.getElementById("product-detail");
-mainProduit.innerHTML = Produit(displayInlist());
-    `<div class="produit">
-    <a href="/produits.html?id=${this.id}">
-    <img src=${this.imageUrl}>
-    <div class="description">
-    <h4 class="produit_title">${this.name}</h4>
-    <p class="produit_description">${this.description}</p>
-    <p class="produit_prix">${this.price / 100}.00 €</p>
-    </div>
-    </a>
-    </div>`
+mainProduit =  Produit.displayInList();
